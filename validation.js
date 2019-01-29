db812586fc9fdee5da7db402c550e7495b7142ba
Laureline <laurelinedev@gmail.com>
2019-01-29 06:54:50 +0100

    handle submit button funcitonnality

--- validation.js
+++ validation.js
@@ -1,4 +1,5 @@
-function checkThat (what) {
+/* Handle changements and actions to do */
+function checkThat (what, e) {
     var regex;
     switch (what) {
         /* 5-12 alphanumeric, any case */
@@ -24,27 +25,49 @@ function checkThat (what) {
             regex = new RegExp('[a-z0-9]{5,}','gi');
             process(what, regex)
             break;
+        default:
+            validateForm(e);
     }
 }
 
+
+/**
+ * Process x input to:
+ *      - check the user input
+ *      - handle error messages
+ */
 function process(input, regex) {
     var match = $(`.${input}`).val().match(regex) ? true : false;
     var $warning = `.${input}.warning`;
     if(!match){
         $($warning).addClass('visible');
     } else {
-        $($warning).removeClass('visible')
+        $($warning).removeClass('visible').addClass('valid');
     }
 }
 
-/**
- * - if all inputs insertions are good, enable submit
- * - clean all input
- */
 
 /**
- * if input
+ * ValidateForm:
+ *      - by showing the submit button disable or enable
+ *      - if all inputs are valid ( with class .valid )
  */
- function submit () {
-     $('input').val('');
+ function validateForm(e){
+    var that = $(e.target)[0].className;
+    var handleButton = $('button').addClass('disable').attr('disabled', 'disabled').html(`Can't submit`);
+    if($('.valid').length !== $('input').length){
+        handleButton;
+    } else {
+        $('button').removeClass('disable').removeAttr('disabled').html('Submit');
+
+        /* Handle cases once valid and then modify input --> back to button disabled */
+        if($('.visible').length !== 0){
+            $(`.${that}`).removeClass('valid');
+            handleButton;
+        }
+    }
  }
+
+ function submit () {
+    $('input').val('');
+}
