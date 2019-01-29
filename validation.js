95d4970ea79b9f6a33de300b18d13257eebcdb50
Laureline <laurelinedev@gmail.com>
2019-01-29 03:30:25 +0100

    inputs checked w/ regexp, style updated

--- validation.js
+++ validation.js
@@ -1,20 +1,48 @@
-const usrN = $('.userName').val();
-const mail = $('.email').val();
-const phone = $('.phone').val();
-const pwd = $('.password').val();
-
-
 function checkThat (what) {
-    console.log(what);
-
+    var regex;
     switch (what) {
-        case 'usrN':
-        break;
-        case 'mail':
-        break;
+        /* 5-12 alphanumeric, any case */
+        case 'username':
+            regex = new RegExp ('[a-z]{5,12}','gi');
+            process(what, regex)
+            break;
+
+        /* alphanumeric, @ and .com or .fr */
+        case 'email':
+            regex = new RegExp('[a-z0-9]+@[a-z]+.(com|fr)$','gi');
+            process(what, regex)
+            break;
+
+        /* 10 digits ( Europe ) */
         case 'phone':
-        break;
-        case 'pwd':
-        break;
+            regex = new RegExp('^[0-9]{10}$','g');
+            process(what, regex)
+            break;
+
+        /* 5 and more alphanumeric */
+        case 'password':
+            regex = new RegExp('[a-z0-9]{5,}','gi');
+            process(what, regex)
+            break;
     }
 }
+
+function process(input, regex) {
+    var verdict = $(`.${input}`).val().match(regex) ? true : false;
+    var $class = `.${input}.warning`;
+    if(!verdict){
+        $($class).addClass('visible');
+    } else {
+        $($class).removeClass('visible');
+    }
+}
+
+/**
+ * - if all inputs insertions are good, enable submit
+ * - clean all input
+ */
+
+
+ function submit () {
+     $('input').val('');
+ }
